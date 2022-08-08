5cff0bd1ee2ece467d70b09794af2cb1374d2901
tash7540 <59746687+tash7540@users.noreply.github.com>
2022-08-08 07:13:49 -0600

    deploy fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 const corsOptions = {
-    origin: 'https://62f04a9b8e5c0c708ae199d9--stellar-praline-5c51c7.netlify.app',
+    origin: 'https://stellar-praline-5c51c7.netlify.app',
     credentials: true,
 };
 app.use(cors(corsOptions));
